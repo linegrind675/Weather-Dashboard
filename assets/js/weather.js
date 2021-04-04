01fdec5e02c49000f29034866fdd1002334516d1
Linegrind <linegrind@icloud.com>
2021-04-03 18:27:45 -0600

    declared all variables

---
+++ assets/js/weather.js
@@ -0,0 +1,23 @@
+let apiKey = "3ac0d8db34de82819d13a9167239acc1";
+let searchBtn = $(".searchBtn");
+let searchInput = $(".searchInput");
+
+//* Create a current date variable
+var today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
+var today = mm + '/' + dd + '/' + yyyy;
+
+//* Right column locations
+let tempEl = $(".temp");
+let humidityEl = $(".humidity");
+let windSpeedEl = $(".windSpeed");
+let uvIndexEl = $(".uvIndex");
+let cardRow = $(".card-row");
+
+//* Left column locations
+let cityNameEl = $(".cityName");
+let currentDateEl = $(".currentDate");
+let weatherIconEl = $(".weatherIcon");
+let searchHistory = $(".historySearch");
