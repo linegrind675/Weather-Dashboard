ba9ea6d783f3621bb3715b890961f99f6a06ff6f
Linegrind <linegrind@icloud.com>
2021-04-07 13:07:47 -0600

    final commit

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -2,13 +2,6 @@ let apiKey = "3ac0d8db34de82819d13a9167239acc1";
 let searchBtn = $(".searchBtn");
 let searchInput = $(".searchInput");
 
-//* Create a current date variable
-var today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0');
-let yyyy = today.getFullYear();
-var today = mm + '/' + dd + '/' + yyyy;
-
 //* Right column locations
 let tempEl = $(".temp");
 let humidityEl = $(".humidity");
@@ -20,7 +13,14 @@ let cardRow = $(".card-row");
 let cityNameEl = $(".cityName");
 let currentDateEl = $(".currentDate");
 let weatherIconEl = $(".weatherIcon");
-let searchHistory = $(".historySearch");
+let searchHistoryEl = $(".historyItems");
+
+//* Create a current date variable
+var today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
+var today = mm + '/' + dd + '/' + yyyy;
 
 if (JSON.parse(localStorage.getItem("searchHistory")) === null) {
     console.log("searchHistory not found")
@@ -29,7 +29,7 @@ if (JSON.parse(localStorage.getItem("searchHistory")) === null) {
     renderSearchHistory();
 }
 
-searchBtn.on("click", function (e) {
+searchBtn.on("click", function(e) {
     e.preventDefault();
     if (searchInput.val() === "") {
         alert("You must enter the name of a city!");
@@ -56,7 +56,7 @@ function renderSearchHistory(cityName) {
     }
 }
 
-function renderWeatherData(cityName, cityHumidity, cityWindSpeed, cityWeatherIcon, uvVal) {
+function renderWeatherData(cityName, cityTemp, cityHumidity, cityWindSpeed, cityWeatherIcon, uvVal) {
     cityNameEl.text(cityName)
     currentDateEl.text(`(${today})`)
     tempEl.text(`Temperature: ${cityTemp} °F`);
@@ -64,7 +64,7 @@ function renderWeatherData(cityName, cityHumidity, cityWindSpeed, cityWeatherIco
     windSpeedEl.text(`Wind Speed: ${cityWindSpeed} MPH`);
     uvIndexEl.text(`UV Index: ${uvVal}`);
     weatherIconEl.attr("src", cityWeatherIcon);
-}
+};
 
 function getWeather(desiredCity) {
     let queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${desiredCity}&APPID=${apiKey}&units=imperial`;
@@ -78,12 +78,13 @@ function getWeather(desiredCity) {
                 cityTemp: weatherData.main.temp,
                 cityHumidity: weatherData.main.humidity,
                 cityWindSpeed: weatherData.wind.speed,
-                cityUVIndex: weatherData.weather[0].icon
+                cityUVIndex: weatherData.coord,
+                cityWeatherIconName: weatherData.weather[0].icon
             }
-            let queryUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${cityObj.cityUVIndex.lat}&lon=${cityObj.cityUVIndex.lon}&APPID=${apiKey}&units=imperial`;
+            let queryUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${cityObj.cityUVIndex.lat}&lon=${cityObj.cityUVIndex.lon}&APPID=${apiKey}&units=imperial`
             $.ajax({
                 url: queryUrl,
-                method: "GET"
+                method: 'GET'
             })
                 .then(function (uvData) {
                     if (JSON.parse(localStorage.getItem("searchHistory")) === null) {
@@ -121,8 +122,10 @@ function getWeather(desiredCity) {
                 })
 
         });
+        getFiveDayForecast();
 
-        getFiveDayForecast() {
+
+        function getFiveDayForecast() {
             cardRow.empty();
             let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${desiredCity}&APPID=${apiKey}&units=imperial`;
             $.ajax({
@@ -130,7 +133,7 @@ function getWeather(desiredCity) {
                 method: "GET"
             })
             .then(function(fiveDayResponse) {
-                for (let i=0; i != fiveDayResponse.list.length; i+=8 ) {
+                for (let i = 0; i != fiveDayResponse.list.length; i+=8 ) {
                     let cityObj = {
                         date: fiveDayResponse.list[i].dt_txt,
                         icon: fiveDayResponse.list[i].weather[0].icon,
@@ -144,7 +147,7 @@ function getWeather(desiredCity) {
                 }
             })
         }
-}
+};
 
 function createForecastCard(date, icon, temp, humidity) {
 
@@ -161,4 +164,4 @@ function createForecastCard(date, icon, temp, humidity) {
     cardHumidity.text(`Humidity: ${humidity} %`);
     fiveDayCardEl.append(cardDate, cardIcon, cardTemp, cardHumidity);
 
-}
+};
